3b5584cbfb9f2c2cfda5183e8b6a8fff128e355f
suryagounder <118352049+suryagounder@users.noreply.github.com>
2023-07-21 16:45:55 +0530

    Update checkoutcode.groovy

--- vars/checkoutcode.groovy
+++ vars/checkoutcode.groovy
@@ -1,6 +1,7 @@
  def call(repoUrl, branch){
-    sh "git clone ${repoUrl} ${workingDir}"
-    sh "git checkout ${branch}"
-    return workingDir
+   def workingDir = "${env.WORKSPACE}"
+   sh "git clone ${repoUrl} ${workingDir}"
+   sh "git checkout ${branch}"
+   return workingDir
  }
 
